898409c6a14b9012e25e41862f34cfa36f001aca
tristan-spear <tspear1704@gmail.com>
2025-12-18 17:21:13 -0800

    finished sign-up and log-in functionality

--- index.js
+++ index.js
@@ -32,10 +32,39 @@ app.get("/sign-up", (req, res) => {
 });
 
 // add new user to db
-app.post("/sign-up-submit", (req, res) => {
+app.post("/sign-up-submit", async (req, res) => {
+    const username = req.body["username"];
+    const password = req.body["password"];
+    const reenterpass = req.body["reenter-password"];
 
+    if(password !== reenterpass)
+    {
+        res.render("sign-up.ejs", { errmessage : "Passwords do not match" });
+        return;
+    }
+    
+    try {
+        // check if username is already taken
+        const result = await db.query (
+            "SELECT * FROM users WHERE name = $1", 
+            [username]
+        );
+        if(result.rowCount !== 0) {
+            res.render("sign-up.ejs", { errmessage : "Username is taken" });
+            return;
+        }
 
-    res.redirect("/log-in");
+        // add username and password to db
+        db.query(
+            "INSERT INTO users (name, password) VALUES ($1, $2);",
+            [username, password]
+        );
+    
+        res.redirect("/log-in");
+    }
+    catch(err) {
+        console.error(err.stack);
+    }
 });
 
 // navigate to log in page
@@ -44,9 +73,33 @@ app.get("/log-in", (req, res) => {
 });
 
 // attempt to log in to account
-app.get("/log-in-submit", (req, res) => {
+app.post("/log-in-submit", async(req, res) => {
 
-    res.render("/home.ejs", { userID : currentUserID });
+    const username = req.body["username"];
+    const password = req.body["password"];
+
+    try {
+        const result = await db.query(
+            "SELECT * FROM users WHERE name = $1",
+            [username]
+        );
+
+        if(result.rowCount === 0) {
+            res.render("log-in.ejs", { errmessage : "Username not found" });
+            return;
+        }
+
+        const userObject = result.rows;
+            
+        if(password == userObject[0].password)
+            res.render("home.ejs");
+    
+        else
+            res.render("log-in.ejs", { errmessage : "Incorrect password" });
+    }
+    catch(err) {
+        console.error(err.stack);
+    }
 });
 
 // add link to db and return shortened version
